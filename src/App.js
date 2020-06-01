aaf669c2408499148c0df2e175d63c9984c33fbd
Shan.tk <tksudharshan@gmail.com>
2020-06-01 09:46:59 +0530

    Basic UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css';
+import { Header } from './components/header'
+import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/incomeexpenses';
+import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
 
 function App() {
   return (
     <div>
-      Hello World
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
     </div>
   );
 }
